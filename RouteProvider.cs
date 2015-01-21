df2811a39da09b113a7ff9013cca482af9dcce9f
Zeeshan Iqbal <zeeshan.waterway@nxvt.com>
2015-01-21 19:40:10 +0500

    Clean up route provider

--- RouteProvider.cs
+++ RouteProvider.cs
@@ -15,34 +15,13 @@ namespace Nop.Plugin.Misc.SimpleCheckout
             #endregion
 
             #region Register Routes
+            //onepagecheckout
             routes.MapRoute("Plugin.Misc.SimpleCheckout.OnePageCheckout",
                 "onepagecheckout/",
                 new { controller = "MiscSimpleCheckout", action = "OnePageCheckout" },
                 new[] { "Nop.Plugin.Misc.SimpleCheckout.Controllers" }
                 );
-            /*
-             //home page
-            routes.MapRoute("Plugin.Misc.FacebookShop.Index",
-                 "facebook/shop/",
-                 new { controller = "MiscFacebookShop", action = "Index" },
-                 new[] { "Nop.Plugin.Misc.FacebookShop.Controllers" }
-            );
 
-            //category page
-            routes.MapRoute("Plugin.Misc.FacebookShop.Category",
-                 "facebook/shop/category/{categoryId}/",
-                 new { controller = "MiscFacebookShop", action = "Category" },
-                 new { categoryId = @"\d+" },
-                 new[] { "Nop.Plugin.Misc.FacebookShop.Controllers" }
-            );
-
-            //search page
-            routes.MapRoute("Plugin.Misc.FacebookShop.ProductSearch",
-                 "facebook/shop/search/",
-                 new { controller = "MiscFacebookShop", action = "Search" },
-                 new[] { "Nop.Plugin.Misc.FacebookShop.Controllers" }
-            );
-             */
             #endregion
         }
 
